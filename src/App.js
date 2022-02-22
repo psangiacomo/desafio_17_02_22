aa5d8be180da725c13d24f333b2896fa8111b9a1
Nahuel Sangiacomo <psangiacomo@adifse.com.ar>
2022-02-22 16:43:54 -0300

    Desafío 22/02/22

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import MainNavigator from './navigation';
 import {Provider} from 'react-redux';
 import React from 'react';
+import {init} from './db';
 import store from './store';
 
 // navigation
+
+init()
+  .then(() => {
+    console.log('DB initialized');
+  })
+  .catch(err => {
+    console.log('DB initialization failed', err);
+  });
 
 export default function App() {
   return (
